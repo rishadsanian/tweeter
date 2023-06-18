56ea3622f21241fa86918270cb7309bdb9cdba39
Rishad Alam <rishad.s.a@gmail.com>
2023-06-18 19:04:38 -0400

    added counter function using jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,20 @@
-console.log("test");
+$(document).ready(function () {
+  counter();
+});
+
+const counter = function () {
+  $("#tweet-text").on("input", function () {
+    //find text of counter
+    let counterText = $(this).siblings(".form-footer").find(".counter");
+    let counterValue = 140 - this.value.length;
+    //assign text of counter to counterValue
+    $(counterText).text(counterValue);
+    //change color of text accordingly
+    if (counterValue < 0) {
+      $(counterText).css({ color: "red" });
+    }
+    if (counterValue >= 0) {
+      $(counterText).css({ color: "black" });
+    }
+  });
+};
