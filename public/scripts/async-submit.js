3bd31978472186cb0f3b077743d7a5bc8b344390
Rishad Alam <rishad.s.a@gmail.com>
2023-06-19 14:19:31 -0400

    error handling with alert

--- public/scripts/async-submit.js
+++ public/scripts/async-submit.js
@@ -1,20 +1,21 @@
 /* eslint-disable no-undef */
 
-const asyncSubmit = function() {
-  $(".form-new").on("submit", function(event) {
+const asyncSubmit = function () {
+  $(".form-new").on("submit", function (event) {
     event.preventDefault();
+    if (!$("#tweet-text").val()) return alert("Empty tweet!");
+    if ($("#tweet-text").val().length > 140) return alert("Tweet too long!");
     const text = $(this).serialize();
-
     $.ajax({
       type: "POST",
       url: "/tweets/",
       data: text,
-      success: function(response) {
+      success: function (response) {
         console.log("Request successful!");
         console.log(response);
         console.log(this.data);
       },
-      error: function(xhr, status, error) {
+      error: function (xhr, status, error) {
         console.log("Request failed!");
         console.log(xhr.responseText);
       },
@@ -22,6 +23,6 @@ const asyncSubmit = function() {
   });
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   asyncSubmit();
 });
